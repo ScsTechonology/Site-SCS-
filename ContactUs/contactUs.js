5539898a5cbcb066d8be7b422a7da7544c91ed43
TheBrue1 <davidcencinato@gmail.com>
2023-02-11 14:33:29 -0300

    attt

--- ContactUs/contactUs.js
+++
@@ -1,9 +0,0 @@
-// Obtendo o formulário
-var form = document.querySelector('form');
-
-// Adicionando um evento de envio ao formulário
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
-  form.style.display = 'none';
-  document.querySelector('.success').style.display = 'block';
-});
